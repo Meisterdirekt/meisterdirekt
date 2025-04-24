54d8a088b0d1e4a7f05749c1858c74f56e1219e1
Meisterdirekt <hendrikberkenstrater@gmail.com>
2025-04-24 05:52:03 +0200

    Create Service.js

---
+++ pages/Service.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+
+const Services = () => {
+  return (
+    <Layout>
+      <h1>Unsere Dienstleistungen</h1>
+      <ul>
+        <li>Sanitär</li>
+        <li>Elektriker</li>
+        <li>Schreiner</li>
+        <li>Malerei</li>
+      </ul>
+    </Layout>
+  );
+};
+
+export default Services;
